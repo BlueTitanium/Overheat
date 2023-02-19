7035cbfd436eb3a49d9287c85337f282dfb7db0b
nirav-golyalla <ng2411@nyu.edu>
2023-02-19 00:52:53 -0500

    Fixed Wandering and Agro behavior

--- OVERHEAT/Assets/Scripts/DroneAI.cs
+++ OVERHEAT/Assets/Scripts/DroneAI.cs
@@ -7,54 +7,72 @@ public class DroneAI : MonoBehaviour
     public float aggroRange = 5f;
     public float fireRange = 3f;
     public float hoverDist = 2f;
-    public float wanderSpeed = 2f;
+    public float wanderSpeed = 3f;
     public float aggroSpeed = 2f;
     public string playerTag = "Player";
 
-    public float wanderRange = 1.0f;
+    public float wanderRange = 5f;
     private float wanderX, startX;
-    private Vector2 startPosition, targetPosition;
+    private Vector2 startPosition, wanderPosition,targetPosition;
 
-    
-    
     public LayerMask ground;
     public Transform elevation;
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = new Vector2(transform.position.x,transform.position.y)
+        startPosition = new Vector2(transform.position.x, transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Transform player = findPlayer(aggroRange);
+        if(player){
+            Approach(player,fireRange);
+        } else{
+            Wander();
+        }
+            
+    }
+
+    //moving drone + tilt and face direction
+    private Vector2 DroneMove(Vector3 target, float speed){
+        Vector2 str = transform.position;
+        Vector2 dir = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        transform.localScale = new Vector3(Mathf.Sign(dir.x-str.x), 1, 1);
+        return dir;
     }
 
     private void Wander(){
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, wanderSpeed * Time.deltaTime);
+        transform.position = DroneMove(wanderPosition,wanderSpeed);
 
         if (transform.position.x == wanderX)
         {
             wanderX = Random.Range(startX - wanderRange, startX + wanderRange);
-            targetPosition = new Vector2(wanderX, transform.position.y);
-            
+            wanderPosition = new Vector2(wanderX, transform.position.y);
         }
     }
 
-    private void Approach(){
+    private void Approach(Transform target, float distance){
+        Vector2 direction = target.position - transform.position;
+        targetPosition = (Vector2)target.position - direction.normalized * distance;
+        transform.position = DroneMove(targetPosition,aggroSpeed);
 
+        if((transform.position - targetPosition).magnitude < 0.01f){
+            Fire();
+        }
     }
 
     private void Fire(){
         //Fire Bullet Prefab Based on Element
     }
 
-    private Transform findPlayer(radius){
+    private Transform findPlayer(float radius){
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
         foreach (Collider2D c in colliders){       
-            if (c.tag == "Player"){
-                return c.Transform
+            print(c.tag);
+            if (c.tag == playerTag){
+                return c.transform;
             }
         }
         return null;
