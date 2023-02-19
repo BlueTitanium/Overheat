6207a6ff85063a7bd60136a7da922bf45dd6e009
nirav-golyalla <ng2411@nyu.edu>
2023-02-19 03:47:48 -0500

    Implemented Firing

--- OVERHEAT/Assets/Scripts/DroneAI.cs
+++ OVERHEAT/Assets/Scripts/DroneAI.cs
@@ -6,21 +6,34 @@ public class DroneAI : MonoBehaviour
 {
     public float aggroRange = 5f;
     public float fireRange = 3f;
-    public float hoverDist = 2f;
     public float wanderSpeed = 3f;
     public float aggroSpeed = 2f;
-    public string playerTag = "Player";
-
     public float wanderRange = 5f;
-    private float wanderX, startX;
-    private Vector2 startPosition, wanderPosition,targetPosition;
+    public float fireLeeway = 0.5f;
+    public float fireCooldown = 4f;
+    private float timeToFire = 0f;
+    public float fireSpeed = 5f;
 
+    public string playerTag = "Player";
+    public string element = "Hot";
+    private string state;
+
+    public float hoverDist = 2f;
+
+    private float wanderX, startX;
+    
+    private Vector2 startPosition, wanderPosition,targetPosition;
+    
+    public Transform suspend,firePoint;
+    public GameObject hot,cold;
     public LayerMask ground;
-    public Transform elevation;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         startPosition = new Vector2(transform.position.x, transform.position.y);
+        state = "Wander";
     }
 
     // Update is called once per frame
@@ -28,23 +41,30 @@ public class DroneAI : MonoBehaviour
     {
         Transform player = findPlayer(aggroRange);
         if(player){
+            state = "Aggro";
             Approach(player,fireRange);
         } else{
+            state = "Wander";
             Wander();
         }
+        timeToFire -= Time.deltaTime; 
             
     }
 
     //moving drone + tilt and face direction
-    private Vector2 DroneMove(Vector3 target, float speed){
+    private Vector2 DroneMove(Vector3 target, float speed, Vector2 aim){
         Vector2 str = transform.position;
         Vector2 dir = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
-        transform.localScale = new Vector3(Mathf.Sign(dir.x-str.x), 1, 1);
+        if(state != "Aggro"){
+            transform.localScale = new Vector3(Mathf.Sign(dir.x-str.x), 1, 1);
+        } else{
+            transform.localScale = new Vector3(Mathf.Sign(aim.x-str.x), 1, 1);
+        }
         return dir;
     }
 
     private void Wander(){
-        transform.position = DroneMove(wanderPosition,wanderSpeed);
+        transform.position = DroneMove(wanderPosition,wanderSpeed,Vector2.zero);
 
         if (transform.position.x == wanderX)
         {
@@ -56,15 +76,25 @@ public class DroneAI : MonoBehaviour
     private void Approach(Transform target, float distance){
         Vector2 direction = target.position - transform.position;
         targetPosition = (Vector2)target.position - direction.normalized * distance;
-        transform.position = DroneMove(targetPosition,aggroSpeed);
+        transform.position = DroneMove(targetPosition,aggroSpeed, (Vector2)target.position);
 
-        if((transform.position - targetPosition).magnitude < 0.01f){
-            Fire();
+        if(((Vector2)transform.position - (Vector2)targetPosition).magnitude < fireLeeway && timeToFire < 0f){
+            timeToFire = fireCooldown;
+            Fire(target);
         }
     }
 
-    private void Fire(){
-        //Fire Bullet Prefab Based on Element
+    private void Fire(Transform target){
+        Vector2 direction = target.position - firePoint.position;
+        GameObject projectile;
+        if(element == "Hot"){
+            projectile = Instantiate(hot, firePoint.position, Quaternion.identity);
+        } else{
+            projectile = Instantiate(cold, firePoint.position, Quaternion.identity);
+        }
+        projectile.transform.rotation = Quaternion.LookRotation(Vector3.forward, direction)* Quaternion.Euler(0, 0, 90);
+        Rigidbody2D projectileRb = projectile.GetComponent<Rigidbody2D>();
+        projectileRb.velocity = direction.normalized * fireSpeed;
     }
 
     private Transform findPlayer(float radius){
@@ -78,11 +108,11 @@ public class DroneAI : MonoBehaviour
         return null;
     }
 
-    private bool CheckGrounded()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(elevation.position, Vector2.down, hoverDist, ground);
-        return hit.collider != null;
-    }
+    // private bool CheckGrounded()
+    // {
+    //     RaycastHit2D hit = Physics2D.Raycast(elevation.position, Vector2.down, hoverDist, ground);
+    //     return hit.collider != null;
+    // }
 
     
 }
